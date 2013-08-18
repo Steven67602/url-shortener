1b877d2505854515a6ad09e41d89e9629a3b5531
Eric Wright <dev@ericwright.me>
2013-08-18 16:11:42 +0900

    Generate Rails boilerplate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = 'a7537175d06d1d1080155763ae98c5d5b32eed0f0a546ad8c6f28a3a78113665df1d170fa8f6447a5692c33b2a8c19a7b15c909c29cf8530761e11ff6899cde7'
